473f1f4352728ff15b335f75fd33bfbfb8be7bd1
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-19 18:08:38 +0100

    fix a bug

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,12 +8,15 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get() //Get /users or /users?role=value
   findAll(@Query('role') role?: 'admin' | 'intern' | 'Engineer  ') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   @Get('interns') //Get /users/interns
@@ -23,11 +26,18 @@ export class UsersController {
 
   @Get(':id') //Get /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id);
   }
 
   @Post() //Post /users
-  create(@Body() user: { any }) {
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINNER' | 'ADMIN';
+    },
+  ) {
     return user;
   }
 
@@ -38,6 +48,6 @@ export class UsersController {
 
   @Delete(':id') //DELETE /users/:id
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id);
   }
 }
