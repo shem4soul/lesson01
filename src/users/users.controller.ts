6a87f9b4e875c9418a48b90c8e19d848ee4d157b
Shittu Emmanuel <shem4soul@gmail.com>
2026-02-20 18:13:53 +0100

    fix a bug

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,8 @@ import {
   ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -31,28 +33,16 @@ export class UsersController {
   }
 
   @Post() //Post /users
-  create(
-    @Body()
-    user: {
-      name: string;
-      email: string;
-      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
-  ) {
-    return this.usersService.create(user);
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
   }
 
   @Patch(':id') //PATCH/users/:id
   update(
     @Param('id', ParseIntPipe) id: number,
-    @Body()
-    userUpdate: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
+    @Body() updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(id, userUpdate);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id') //DELETE /users/:id
